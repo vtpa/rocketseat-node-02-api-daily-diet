e3a4c1b244cade6b3571793312100418f6330283
Vinicius Passos <viniciuspa87@hotmail.com>
2023-05-08 13:01:39 -0300

    CHORE: app and server improvements

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { mealsRoutes } from './routes/meals'
+import { usersRoutes } from './routes/users'
 
 export const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello Rudolf!'
+app.register(cookie)
+
+app.register(mealsRoutes, {
+  prefix: 'meals',
+})
+app.register(usersRoutes, {
+  prefix: 'users',
 })
